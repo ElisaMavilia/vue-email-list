4044076e62eeaad9fbb572744a041c9edf601b1f
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-08 17:35:52 +0200

    adjustments

--- js/script.js
+++ js/script.js
@@ -8,24 +8,23 @@ createApp({
             loading: false //inizializzo variabile loading a false
         }
     },
-    methods:{
-        getRandomEmails(){ //funzione per creare l'email random
+    methods: {
+        getRandomEmails() { //funzione per creare l'email random
             this.loading = true;
             const list = []; //array vuoto in cui verranno pushati gli elementi tutti in una volta
-            for(let i = 0; i < this.numEmails; i++){ //la chiamata ad Axios va fatta all'interno del ciclo for per reiterare 10 volte (numEmails)
+            for (let i = 0; i < this.numEmails; i++) { //la chiamata ad Axios va fatta all'interno del ciclo for per reiterare 10 volte (numEmails)
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
                     console.log(resp.data.response);
                     const email = resp.data.response; //creo una variabile con la chiave response che contiene l'email random
                     list.push(email); // pusho l'email random dentro l'array
                     this.loading = false; //per mostrare la lista, loading deve tornare false
-                    if(i === this.numEmails - 1){ // condizione che controlla se l'indice corrisponde alla lunghezza preimpostata meno 1 (in quanto l'indice parte da zero)
+                    if (i === this.numEmails - 1) { // condizione che controlla se l'indice corrisponde alla lunghezza preimpostata meno 1 (in quanto l'indice parte da zero)
                         this.randomEmails = [...list]; //con lo spread si crea la copia di dell'oggetto list
                     }
                 });
             }
             console.log(this.randomEmails);
         }
-       
     },
     created() {
         this.getRandomEmails();
